c6e4f42818f47fd05af49825e0af9d188f5f99ee
almirrps <almirrps@hotmail.com>
2023-01-10 18:01:37 -0300

    Criando métodos de validação no endpoint save()

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -2,10 +2,13 @@ package com.api.parkingcontrol.repositories;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository   //Coloquei essa anotation por questão de clareza, mas ela já é embutida no JpaRepository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
-
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
 }
